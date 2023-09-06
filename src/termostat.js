f4b75a0de5787b41871658bc35c187af16c57719
CoolerMgdn <Kamil.sadykov@mail.ru>
2023-09-06 19:06:47 +0300

    123213

---
+++ src/termostat.js
@@ -0,0 +1,29 @@
+jQuery(document).ready(function($){
+	//open popup
+	$('.cd-popup-trigger').on('click', function(event){
+		event.preventDefault();
+		$('.cd-popup').addClass('is-visible');
+	});
+	
+	//close popup
+	$('.cd-popup').on('click', function(event){
+		if( $(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup') ) {
+			event.preventDefault();
+			$(this).removeClass('is-visible');
+		}
+	});
+	//close popup when clicking the esc keyboard button
+	$(document).keyup(function(event){
+    	if(event.which=='27'){
+    		$('.cd-popup').removeClass('is-visible');
+	    }
+    });
+});
+
+$("#myRange").roundSlider({
+  sliderType: "min-range",
+  handleShape: "round",
+  width: 22, // width of the roundSlider
+  radius: 100, // radius size
+  value: 60 // value you want to apply
+});
